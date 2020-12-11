a615aba88ee123353248aa49d14e2126d8431fb5
Jussi Kinnula <jussi.kinnula@wunderdog.fi>
2020-12-11 15:19:42 +0200

    Changes to get basic stuff working

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,29 @@
-import fs from 'fs'
-import path from 'path'
-import { ApolloServer, gql } from 'apollo-server'
+import { ApolloServer } from 'apollo-server'
+import { createApplication } from 'graphql-modules'
+import Database from './models/Database';
+import AssignmentModule from './modules/assignment'
+import OperationsModule from './modules/operations';
 
-// Load schema from an external file (relative to build directory).
-const schema = fs.readFileSync(path.join(__dirname, '..', 'schema.graphql'))
-const typeDefs = gql`${schema}`
+(async function start(): Promise<void> {
+  // Initialize GraphQL modules
+  const application = createApplication({
+    modules: [
+      AssignmentModule,
+      OperationsModule
+    ]
+  })
 
-// Define resolvers to load
-const resolvers = {
-  Query: {
-    hello: () => 'Hello, world!'
-  },
-}
+  // This is the aggregated schema
+  const schema = application.createSchemaForApollo()
 
-async function start() {
+  // Initialize database. Sequelize creates tables on bootstrapping
+  const db = Database.instance
+  await db.init()
+
   // Start the server
-  const server = new ApolloServer({ typeDefs, resolvers })
+  const server = new ApolloServer({
+    schema
+  })
   const { url } = await server.listen()
   console.log(`🚀 Server ready at ${url}`)
-}
-
-start()
+})();
